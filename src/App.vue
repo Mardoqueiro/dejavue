c0e652a73e7e343fe3653a0740d1e46daf77837e
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-06-21 11:12:32 +0200

    card-comp

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <template>
- <div>
-   
+ <div class="container">
+  <div class="row">
+    <h1>Dejavue</h1>
+  </div>
+  <div class="row">
+    <Friends/>
+  </div>
+  <div class="row">
+    <Products/>
+  </div>
  </div>
   
 </template>
 
 <script>
-
+import Friends from '@/components/Friends.vue'
+import Products from '@/components/Products.vue'
 export default {
   name: 'App',
   components: {
+    Friends,
+    Products,
+  },
+  data() {
+    return {
+      friends: [
+      {
+        firstName: "Mardoqueiro",
+        lastName: "Dimbu",
+        profile: 'https://mardoqueiro.github.io/all_images/E-com_/Team-photo/Gitprofil.jpg'
+      },
+      {
+        firstName: "Ordi",
+        lastName: "Kitoko",
+        profile: 'https://mardoqueiro.github.io/all_images/E-com_/Team-photo/IMG_20240607_135942_589.jpg'
+      },
+      {
+        firstName: "Terence",
+        lastName: "Tweng",
+        profile: 'https://mardoqueiro.github.io/all_images/E-com_/Team-photo/IMG_20240607_150343_596.jpg'
+      },
+      {
+        firstName: "Kamva",
+        lastName: "Banzi",
+        profile: 'https://mardoqueiro.github.io/all_images/E-com_/Team-photo/Screenshot_20240607_150245_WhatsApp.jpg'
+      },
+      ]
+    }
   }
 }
 </script>
@@ -22,5 +59,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.card {
+  display: grid;
 }
 </style>
---
+++ src/components/Friends.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="row">
+        <div v-if="friends.length">
+          <Card v-for="friend in friends" :key="friend.id" :gender="friend.gender">
+            <template #cardheader>
+                <img class="img-fluid" :src="friend.profile" :alt="friend.firstName" loading="lazy">
+              {{ friend.firstName }} {{ friend.lastName }}
+              </template>
+            <template #cardbody>
+              <p>Gender: {{ friend.gender }}</p>
+              <p>Profile: {{ friend.profile }}</p>
+            </template>
+          </Card>
+        </div>
+        <div v-else>
+          <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  import Card from './Card.vue';
+  
+  export default {
+    name: 'FriendComp',
+    components: {
+      Card,
+    },
+    data() {
+      return {
+        friends: [
+      {
+        id: 1,
+        firstName: "Mardoqueiro",
+        lastName: "Dimbu",
+        gender: "male",
+        profile: 'https://mardoqueiro.github.io/all_images/E-com_/Team-photo/Gitprofil.jpg'
+      },
+      {
+        id: 2,
+        firstName: "Ordi",
+        lastName: "Kitoko",
+        gender: "female",
+        profile: 'https://mardoqueiro.github.io/all_images/E-com_/Team-photo/IMG_20240607_135942_589.jpg'
+      },
+      {
+        id: 3,
+        firstName: "Terence",
+        lastName: "Tweng",
+        gender: "male",
+        profile: 'https://mardoqueiro.github.io/all_images/E-com_/Team-photo/IMG_20240607_150343_596.jpg'
+      },
+      {
+        id: 4,
+        firstName: "Kamva",
+        lastName: "Banzi",
+        gender: "female",
+        profile: 'https://mardoqueiro.github.io/all_images/E-com_/Team-photo/Screenshot_20240607_150245_WhatsApp.jpg'
+      },
+      ]
+      };
+    },
+  };
+  </script>
+  
